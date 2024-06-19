23dd10f490b860e5ea33839f3f4fc6a583ed1683
Ashlin Alex <151556292+ashlinalex1@users.noreply.github.com>
2024-06-19 22:06:13 +0530

    Update tower.c

--- tower.c
+++ tower.c
@@ -1,28 +1,24 @@
-#include <stdio.h>
-
-// Function to move n disks from source to destination using auxiliary
-void toh(int n, char source, char destination, char auxiliary) {
-    // Base case: If only one disk, move it from source to destination
-    if (n >= 1) {
-        // Move n-1 disks from source to auxiliary using destination as auxiliary
-        toh(n - 1, source, auxiliary, destination);
-        
-        // Move the nth disk from source to destination
-        printf("\nMove disk %d from rod %c to rod %c", n, source, destination);
-        
-        // Move the n-1 disks from auxiliary to destination using source as auxiliary
-        toh(n - 1, auxiliary, destination, source);
-    }
+#include<stdio.h>
+#include<conio.h>
+void towersofhanoi(int n,char fromTower,char toTower,char auxTower)
+{
+	if(n==1)
+	{
+		printf("\n Move disk 1 from %c tower to %c tower \n",fromTower,toTower);
+	}
+	else
+	{
+		towersofhanoi(n-1,fromTower,auxTower,toTower);
+		printf("\n Move disk %d from %c tower to %c tower \n",n,fromTower,toTower);
+		towersofhanoi(n-1,auxTower,toTower,fromTower);
+	}
 }
-
-int main() {
-    int n; // Number of disks
-
-    printf("Enter the number of disks: ");
-    scanf("%d", &n);
-
-    // A, B and C are names of rods
-    toh(n, 'A', 'C', 'B');
-
-    return 0;
+void main()
+{
+	int n;
+	clrscr();
+	printf("Enter the no of disk :");
+	scanf("%d", &n);
+	towersofhanoi(n,'A','C','B');
+	getch();
 }
